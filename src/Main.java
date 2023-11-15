576807e12e02b3d33f0c9f755646bb950717479e
olivia <oliviaw2026@student.cis.edu.hk>
2023-11-15 08:59:15 +0800

    Removed redundant comments.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
 public class Main {
-
     /**
      * Given two arrays of integers, write a method called combine that combines the two arrays into one larger array.
      * The method combine will return a new one dimensional array of integers.
@@ -8,9 +7,17 @@ public class Main {
      * zip(myArray1, myArray2) → {1,3,5,7,9,2,4,6,8,10,12,14,16}
      */
     public static int[] combine(int[] array1, int[] array2) {
-
-        return null;
-
+        int[] combinedArray = new int[array1.length+array2.length];
+                int index = 0;
+                for (int i=0; i<array1.length; i++) {
+                    combinedArray[index] = array1[i];
+                    index++;
+                }
+        for (int i=0; i<array2.length; i++) {
+            combinedArray[index] = array2[i];
+            index++;
+        }
+        return combinedArray;
     }
 
     /**
@@ -21,8 +28,15 @@ public class Main {
      * zip(myArray1, myArray2) → {1,2,3,4,5,6,7,8,9,10}
      */
     public static int[] zip(int[] array1, int[] array2) {
-
-        return null;
+        int[] zipArray = new int[array1.length+array2.length];
+        int index = 0;
+        for (int i=0; i<array1.length; i++) {
+            zipArray[index]=array1[i];
+            index++;
+            zipArray[index]=array2[i];
+            index++;
+        }
+        return zipArray;
 
     }
 
@@ -34,8 +48,13 @@ public class Main {
      * product(myArray1, myArray2) → {2,12,30,56,90}
      */
     public static int[] product(int[] array1, int[] array2) {
-
-        return null;
+        int[] productArray = new int[array1.length];
+        int index = 0;
+        for (int i=0; i<array1.length; i++) {
+            productArray[index] = array1[i]*array2[i];
+            index++;
+        }
+        return productArray;
 
     }
 
@@ -56,15 +75,21 @@ public class Main {
      * capitalCount(words) → {1, 2, 2, 0}
      */
     public static int[] capitalCount(String[] words) {
-
+        for (int i=0; i<words.length; i++) {
+            String word = words[i];
+            countCapitalLetters(word);
+        }
         return null;
-
     }
 
     public static int countCapitalLetters(String word) {
-
-        return 0;
-
+        int count = 0;
+        for (int i=0; i<word.length(); i++) {
+            if (Character.isUpperCase(word.charAt(i))) {
+                count++;
+            }
+        }
+        return count;
     }
 
 }
